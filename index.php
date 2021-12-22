462f4e024315afdaa54fa91828b26b429f9099a1
Jackson Almeida <jacksonnalmeida30@gmail.com>
2021-12-21 22:02:09 -0400

    contrutores

--- index.php
+++ index.php
@@ -4,39 +4,61 @@ class Login
 {
     private $email;
     private $senha;
+    private $nome;
 
-    public function  getEmail(){
+    public function __construct($email, $senha, $nome)
+    {
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->senha = $senha;
+    }
+
+    public function  getEmail()
+    {
         return $this->email;
     }
 
-    public function setEmail($email){
+    public function setEmail($email)
+    {
         $email = filter_var($email, FILTER_SANITIZE_EMAIL);
         $this->email = $email;
     }
 
-    public function getSenha(){
+    public function getSenha()
+    {
         return $this->senha;
     }
 
-    public function setSenha($senha){
+    public function setSenha($senha)
+    {
         $this->senha = $senha;
     }
 
-    public function logar(){
-        if($this->email == "test@gmail.com" and $this->senha == "123456"){
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function logar()
+    {
+        if ($this->email == "test@gmail.com" and $this->senha == "123456") {
             echo "Logado com sucesso";
-        }
-        else{
+        } else {
             echo "Dados inválidos";
         }
     }
 }
 
-$logar = new Login();
-$logar->setEmail("test@gmail.com");
-$logar->setSenha("123456");
+$logar = new Login("test@gmail.com", "123456", "Jackson Almeida");
 $logar->logar();
 echo "<pre>";
 echo $logar->getEmail();
 echo "<br>";
 echo $logar->getSenha();
+echo "<br>";
+echo $logar->getNome();
