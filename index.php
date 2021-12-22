37eae5295c4d8386f679a7874ec99fafacaafbd2
Jackson Almeida <jacksonnalmeida30@gmail.com>
2021-12-21 21:27:35 -0400

    utilizando metodos de uma class

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
-<?php 
+<?php
 
-class Pessoa{
-    
+class Pessoa
+{
+
     public $nome;
     public $idade;
 
-    public function falar(){
-        echo "falou";
+    public function falar()
+    {
+        echo $this->nome . " de " . $this->idade . " acabaou de falar";
     }
 }
 
 $jackson = new Pessoa();
+$jackson->nome = "Jackson Almeida";
+$jackson->idade = 23;
 $jackson->falar();
