7d8f288cae58a547c5598f2e70bea1a674c4dbd9
Jackson Almeida <jacksonnalmeida30@gmail.com>
2021-12-21 21:46:33 -0400

    usando get e set

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
 <?php
 
-class Pessoa
+class Login
 {
+    private $email;
+    private $senha;
 
-    public $nome;
-    public $idade;
+    public function  getEmail(){
+        return $this->email;
+    }
 
-    public function falar()
-    {
-        echo $this->nome . " de " . $this->idade . " acabaou de falar";
+    public function setEmail($email){
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $this->email = $email;
+    }
+
+    public function getSenha(){
+        return $this->senha;
+    }
+
+    public function setSenha($senha){
+        $this->senha = $senha;
+    }
+
+    public function logar(){
+        if($this->email == "test@gmail.com" and $this->senha == "123456"){
+            echo "Logado com sucesso";
+        }
+        else{
+            echo "Dados inválidos";
+        }
     }
 }
 
-$jackson = new Pessoa();
-$jackson->nome = "Jackson Almeida";
-$jackson->idade = 23;
-$jackson->falar();
+$logar = new Login();
+$logar->setEmail("test@gmail.com");
+$logar->setSenha("123456");
+$logar->logar();
+echo "<pre>";
+echo $logar->getEmail();
+echo "<br>";
+echo $logar->getSenha();
